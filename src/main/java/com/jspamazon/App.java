b57e8c53c3c2ad1fadaa717e279744eae148296b
WRATHRAVAN <niteshsudhakar333@gmail.com>
2023-12-13 14:14:27 +0530

    changed made

---
+++ src/main/java/com/jspamazon/App.java
@@ -0,0 +1,14 @@
+package com.jspamazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+//    first change
+}
